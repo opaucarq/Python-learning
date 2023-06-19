0dc59c6f8310a1dbfe9c6ae797d0e84f34ea9ce6
Oliver <opaucarq@uni.pe>
2023-06-18 23:14:19 -0500

    add: config general game

---
+++ Games/Game03-PokemonPuzzle/cfg.py
@@ -0,0 +1,18 @@
+import os
+WD = os.getcwd
+
+SCREENSIZE = (640, 640)
+
+PICTURE_ROOT_DIR = os.path.join(WD, 'resources/pictures')
+
+FONT_PATH = os.path.join(WD, 'resources/font/FZSTK.TTF')
+
+BGCOLOR = (255, 255, 255)
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+FPS = 40
+
+RANDNUM = 100
